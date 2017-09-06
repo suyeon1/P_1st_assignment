6493ebf9dbec43ad4e97b2e5419e8e933201a18e
suyeon1 <31675726+suyeon1@users.noreply.github.com>
2017-09-06 10:30:54 +0900

    Add files via upload

---
+++ P_1st_assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{
+ 
+       printf("2017301012\n");
+       printf("±è¼ö¿¬\n");
+       printf("01051785109\n");
+       printf("thdcjsryghl@naver.com\n");
+
+       return 0;
+}
